b594b68060c2cfeed6542e08fbfe74382fced61e
anoop <anoop@tuxera.com>
2019-07-07 21:30:55 +0300

    bootstrap.sh: Install and instantiate chaincode for org1 - lab 4

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,20 +7,11 @@
 export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 CHANNEL_NAME=allarewelcome
-
-# clean up and start
-docker rm couchdb0.org1.example.com couchdb1.org1.example.com
-docker-compose -f docker-compose.yml up -d couchdb0.org1.example.com couchdb1.org1.example.com peer0.org1.example.com peer1.org1.example.com
-
-# wait for Hyperledger Fabric to start
-# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=5
-#echo ${FABRIC_START_TIMEOUT}
-sleep ${FABRIC_START_TIMEOUT}
+COMPOSE_PROJECT_NAME=startfiles
 
 set -ev
-# Join peer0.org1.example.com and peer1.org1.example.com to the channel.
-docker exec peer0.org1.example.com peer channel fetch oldest allarewelcome.block -c allarewelcome -o orderer.example.com:7050
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b allarewelcome.block
-docker exec peer1.org1.example.com peer channel fetch oldest allarewelcome.block -c allarewelcome -o orderer.example.com:7050
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b allarewelcome.block
+# Install chaincode on peer0.org1.example.com and peer1.org1.example.com.
+docker exec cli peer chaincode install -n sacc -v 1.0 -p github.com/sacc
+docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" cli peer chaincode install -n sacc -v 1.0 -p github.com/sacc
+# Instantiate chaincode from peer0 (anchor)
+docker exec cli peer chaincode instantiate -n sacc -v 1.0 -C allarewelcome -c '{"Args":["Match", "50"]}' --policy "AND('org1.peer', OR ('org1.member'))"
