996b5c6369977f5625a2f136cc4c4a282660f6b6
anoop <anoop@tuxera.com>
2019-07-08 11:45:32 +0300

    bootstrap.sh, configtx.yaml: Updated anchor peer from peer0 -> peer1 - lab 5

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,9 @@ CHANNEL_NAME=allarewelcome
 COMPOSE_PROJECT_NAME=startfiles
 
 set -ev
-# Install chaincode on peer0.org1.example.com and peer1.org1.example.com.
-docker exec cli peer chaincode install -n sacc -v 1.0 -p github.com/sacc
-docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" cli peer chaincode install -n sacc -v 1.0 -p github.com/sacc
-# Instantiate chaincode from peer0 (anchor)
-docker exec cli peer chaincode instantiate -n sacc -v 1.0 -C allarewelcome -c '{"Args":["Match", "50"]}' --policy "AND('org1.peer', OR ('org1.member'))"
+configtxgen -profile OneOrgChannel -asOrg Org1MSP -outputAnchorPeersUpdate ./config/anchorUpdate.tx -channelID $CHANNEL_NAME
+# re-create cli container
+docker-compose stop cli
+docker-compose up -d cli
+# Update anchor peer from peer0 -> peer1
+docker exec cli peer channel update -c $CHANNEL_NAME -f /etc/hyperledger/configtx/anchorUpdate.tx -o orderer.example.com:7050
