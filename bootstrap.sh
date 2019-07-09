a9c44fc3557cf328044faa0f912ba5cd1b44b586
anoop <anoop@tuxera.com>
2019-07-09 15:34:51 +0300

    configtx.yaml, docker-compose.yml, bootstrap.sh: Orderer solo -> kafka - lab 9

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,20 +7,16 @@
 export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 COMPOSE_PROJECT_NAME=startfiles
+CHANNEL_NAME=allarewelcome
 
 set -ev
-# Create org1 only channel
-configtxgen -profile Org1Channel -outputCreateChannelTx ./config/org1channel.tx -channelID org1channel
-docker exec cli peer channel create -f /etc/hyperledger/configtx/org1channel.tx -c org1channel -o orderer.example.com:7050
-docker exec -e CORE_PEER_ADDRESS=peer1.org1.example.com:7051 -e CORE_PEER_LOCALMSPID=Org1MSP -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp cli peer channel join -b org1channel.block
+# start kakfa and zookeeper
+docker-compose up -d kafka0.example.com kafka1.example.com zoo0.example.com zoo1.example.com zoo2.example.com
+docker-compose up -d ca.example.com orderer.example.com peer0.org1.example.com peer1.org1.example.com peer0.org2.example.com peer1.org2.example.com cli
 
-# Create org2 only channel
-configtxgen -profile Org2Channel -outputCreateChannelTx ./config/org2channel.tx -channelID org2channel
-docker exec -e CORE_PEER_ADDRESS=peer1.org2.example.com:7051 -e CORE_PEER_LOCALMSPID=Org2MSP -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp cli peer channel create -f /etc/hyperledger/configtx/org2channel.tx -c org2channel -o orderer.example.com:7050
-docker exec -e CORE_PEER_ADDRESS=peer1.org2.example.com:7051 -e CORE_PEER_LOCALMSPID=Org2MSP -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp cli peer channel join -b org2channel.block
-
-# Create org1+org2 channel
-configtxgen -profile Org1Org2Channel -outputCreateChannelTx ./config/org1org2channel.tx -channelID org1org2channel
-docker exec cli peer channel create -f /etc/hyperledger/configtx/org1org2channel.tx -c org1org2channel -o orderer.example.com:7050
-docker exec -e CORE_PEER_ADDRESS=peer1.org1.example.com:7051 -e CORE_PEER_LOCALMSPID=Org1MSP -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp cli peer channel join -b org1org2channel.block
-docker exec -e CORE_PEER_ADDRESS=peer1.org2.example.com:7051 -e CORE_PEER_LOCALMSPID=Org2MSP -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp cli peer channel join -b org1org2channel.block
+# Re-join CHANNEL_NAME to all peers
+docker exec cli peer channel fetch 0 allarewelcome.block -o orderer.example.com:7050 -c $CHANNEL_NAME
+docker exec cli peer channel join -b allarewelcome.block
+docker exec -e CORE_PEER_ADDRESS=peer1.org1.example.com:7051 cli peer channel join -b allarewelcome.block
+docker exec -e CORE_PEER_LOCALMSPID=Org2MSP -e CORE_PEER_ADDRESS=peer0.org2.example.com:7051 -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp cli peer channel join -b allarewelcome.block
+docker exec -e CORE_PEER_LOCALMSPID=Org2MSP -e CORE_PEER_ADDRESS=peer1.org2.example.com:7051 -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp cli peer channel join -b allarewelcome.block
