1789b2f143a28ff105d4cae20d4332e377ab2d6d
anoop <anoop@tuxera.com>
2019-07-14 14:07:44 +0300

    bootstrap.sh, docker-compose.yml: Enable tls for peer0 - lab 11

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,58 +8,17 @@ export PATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin:${PWD}/../bin:${
 export FABRIC_CFG_PATH=${PWD}
 CHANNEL_NAME=allarewelcome
 
-# remove previous crypto material and config transactions
-rm -fr config
-rm -fr crypto-config
-mkdir config
-
-# generate crypto material
-cryptogen generate --config=./crypto-config.yaml
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate crypto material..."
-  exit 1
-fi
-
-# generate genesis block for orderer
-configtxgen -profile OneOrgOrdererGenesis -outputBlock ./config/genesis.block
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate orderer genesis block..."
-  exit 1
-fi
-
-# generate channel configuration transaction
-configtxgen -profile OneOrgChannel -outputCreateChannelTx ./config/allarewelcome.tx -channelID allarewelcome
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate channel configuration transaction..."
-  exit 1
-fi
-
-# generate anchor peer transaction
-configtxgen -profile OneOrgChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID allarewelcome -asOrg Org1MSP
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org1MSP..."
-  exit 1
-fi
-
-
-sleep 5s
-
 set -ev
 
-# don't rewrite paths for Windows Git Bash users
-export MSYS_NO_PATHCONV=1
+docker-compose -f docker-compose.yml up -d peer0.org1.example.com cli
 
-docker-compose -f docker-compose.yml down
-
-docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.org1.example.com
-
-# wait for Hyperledger Fabric to start
-# incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=10
-#echo ${FABRIC_START_TIMEOUT}
+export FABRIC_START_TIMEOUT=5
 sleep ${FABRIC_START_TIMEOUT}
 
-# Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c allarewelcome -f /etc/hyperledger/configtx/allarewelcome.tx
-# Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b allarewelcome.block
+# fetch and join a channel
+docker exec -e CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp peer0.org1.example.com peer channel fetch 0 allarewelcome.block -o orderer.example.com:7050 -c allarewelcome
+docker exec -e CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp peer0.org1.example.com peer channel join -b allarewelcome.block
+
+# chaincode query with tls
+export cert_base=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin\@org1.example.com/tls
+docker exec cli peer channel list --tls --cafile $cert_base/ca.crt --keyfile $cert_base/client.key --certfile $cert_base/client.crt
